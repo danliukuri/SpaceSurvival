bb845ff5177c72e64489e1cbcf2324aaccc58366
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2021-02-23 22:14:16 +0200

    Create project Import space tiles Make player controller

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float tilt;
+
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, 0f, Input.GetAxis("Vertical") * speed);
+        rigidbody.rotation = Quaternion.Euler(rigidbody.velocity.z * tilt, 0f, rigidbody.velocity.x * -tilt);
+    }
+}
