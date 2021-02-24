06588d58821b7fa8356e348fa86221554af45742
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2021-02-24 18:57:22 +0200

    Make camera following for player

---
+++ Assets/Scripts/CopyPosition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyPosition : MonoBehaviour
+{
+	[Header("Parameters")]
+	[SerializeField] Transform target;
+	[SerializeField] float speed;
+
+	Vector3 offset;
+
+	// Use this for initialization
+	void Start()
+	{
+		offset = transform.position;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		if (target)
+		{
+			transform.position = Vector3.Lerp(transform.position, target.TransformPoint(offset), speed);
+		}
+	}
+}
