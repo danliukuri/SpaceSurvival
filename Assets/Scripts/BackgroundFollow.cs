08c7a2e38f7e5e93a39a90227513f7e595fb75dc
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2021-03-17 01:00:52 +0200

    Add a cool space background with parallax effect

---
+++ Assets/Scripts/BackgroundFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class BackgroundFollow : MonoBehaviour
+{
+    #region Fields
+    [SerializeField] float parralax;
+
+	Material material;
+	Vector2 offset;
+    #endregion
+
+    #region Methods
+    private void Start()
+    {
+		material = GetComponent<MeshRenderer>().material;
+		offset = material.mainTextureOffset;
+	}
+    void FixedUpdate ()
+	{
+		offset.x = transform.position.x / transform.localScale.x / parralax;
+		offset.y = transform.position.z / transform.localScale.y / parralax;
+
+		material.mainTextureOffset = offset;
+	}
+    #endregion
+}
